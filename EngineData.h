0f0d5e687fbca364358eae8320d158ed5790aa67
Krueger "Mac" Williams <williamskm@msoe.edu>
2024-12-12 16:12:06 -0600

    Committed Data Structure

--- EngineData.h
+++ EngineData.h
@@ -4,5 +4,19 @@
 
 typedef struct {
     pthread_mutex_t engine_mutex;
-
+    float voltage_position;
+    float voltage_speed;
+    int cruise;
+    int speed_setp;
+    int speed_actual;
+    int position_setp;
+    int position_actval;
+    float accel_man;
+    float break_man;
+    float accel_command;
+    float break_command;
+    float feet_position;
+    float mph_speed;
+    float accel_volt;
+    float break_volt;
 } EngineData;
